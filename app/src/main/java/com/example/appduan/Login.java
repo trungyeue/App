057c2eec3b92f1450ae9010f1b7c507cf1cb2261
= <=>
2019-09-25 10:05:58 +0700

    up

---
+++ app/src/main/java/com/example/appduan/Login.java
@@ -0,0 +1,22 @@
+package com.example.appduan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+}
