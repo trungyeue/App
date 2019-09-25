74539ce0cd21b64e6a9377a9cedf1d51a8037044
= <=>
2019-09-25 10:05:26 +0700

    up

--- app/src/main/java/com/example/appduan/MainActivity.java
+++ app/src/main/java/com/example/appduan/MainActivity.java
@@ -1,14 +1,71 @@
 package com.example.appduan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+Button inHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        inHome=findViewById(R.id.inhome);
+//        inHome.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(MainActivity.this,InHome.class));
+//
+//            }
+//        });
+       }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.reset:
+                return true;
+            case R.id.logout:
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Bạn có muốn đăng xuất không?");
+                builder.setCancelable(false);
+                builder.setPositiveButton("Quay lại", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Không thoát được", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.setNegativeButton("Đăng xuất", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent iLogin = new Intent(MainActivity.this, Login.class);
+                        startActivity(iLogin);
+
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+                return true;
+
+        }
+        return true;
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.item,menu);
+        return true;
     }
 }
